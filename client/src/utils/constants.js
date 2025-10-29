6b4bb8c171814f7b4997e2bab4c21ca0854e5816
Aayush Jamwal <aayushjamwal@gmail.com>
2025-10-29 14:21:08 +0530

    first commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI=abi.abi;
+export const contractAddress='0x463C8C51C88b51724016C3cFC6D604E1F69da053';
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,12 @@
+//
+require('@nomiclabs/hardhat-waffle');
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url:'https://eth-goerli.g.alchemy.com/v2/Qu6F3GMhYJ_4W-cXu9ticUvXMsp5Vt_0',
+      accounts: ['234e2f82bc425217a194c55f0d0789db352bc66e3b3eda93ae2d8759f60944f3']
+    },
+  },
+};
+
